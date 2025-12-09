6db8aad58cd67dfe543e207e3a89fad4002e2bd3
lucas178 <60259486+lucas178@users.noreply.github.com>
2025-12-08 22:36:50 -0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { BackArrowIcon } from './Icons';
 
@@ -10,10 +9,10 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ title, onBack }) => {
   return (
     <div className="relative flex items-center justify-center p-4">
-      <button onClick={onBack} className="absolute left-4 text-slate-600 hover:text-pink-500 transition-colors">
+      <button onClick={onBack} className="absolute left-4 text-slate-600 dark:text-slate-300 hover:text-pink-500 dark:hover:text-pink-400 transition-colors">
         <BackArrowIcon className="h-8 w-8" />
       </button>
-      <h1 className="text-2xl font-bold text-slate-700 tracking-wider uppercase">{title}</h1>
+      <h1 className="text-2xl font-bold text-slate-700 dark:text-white tracking-wider uppercase">{title}</h1>
     </div>
   );
 };
